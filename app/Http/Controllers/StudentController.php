f21ace1620c2f25332cefdf48acb73f1ce12fa02
Harsh0726 <harshanineranjana@gmail.com>
2024-02-10 10:37:20 +0530

    created student record create method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    // public function __construct()
+    // {
+    //     $this->middleware(function ($request, $next) {
+    //         $request->headers->set('Accept', 'application/json');
+    //         return $next($request);
+    //     });
+    // }
+
+    public function create(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:students',
+            'phone_number' => 'required|string',
+            'gender' => 'required|in:male,female,other',
+            'date_of_birth' => 'required|date',
+            'address' => 'required|string',
+        ]);
+
+        // Create a new student record
+        $student = Student::create($validatedData);
+
+        // Return a JSON response
+        return response()->json(['message' => 'Student created successfully', 'data' => $student], 201);
+    }
+
 }
